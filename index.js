1510a6a021101ca5c98b832b01c0b0e10d185224
12q1 <thomasham89@gmail.com>
2020-10-17 20:28:21 +0200

    added command handler

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 const Discord = require('discord.js');
-const config = require('./config.json');
+const { prefix, token } = require('./config.json');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === `${config.prefix}ping`) {
-    msg.reply('pong');
-  }
+client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (command === 'ping') {
+        message.channel.send('pong');
+    } 
+    else if (command === 'beep') 
+    {
+        message.channel.send('boop');
+    }
+    else if (command === 'args-info') 
+    {
+        if (!args.length) {
+            return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+        }
+    
+        message.channel.send(`Command name: ${command}\nArguments: ${args}`);
+    }
+    // other commands...
 });
 
-client.login(config.token);
+client.login(token);
