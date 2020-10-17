07beb7395236dc68c80e7beb7bddb85a7d365938
12q1 <thomasham89@gmail.com>
2020-10-17 21:36:14 +0200

    added wiki command

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const Discord = require('discord.js');
+const { search } = require('superagent');
+const superagent = require('superagent');
 const { prefix, token } = require('./config.json');
 const client = new Discord.Client();
 
@@ -14,18 +16,33 @@ client.on('message', message => {
 
     if (command === 'ping') {
         message.channel.send('pong');
-    } 
-    else if (command === 'beep') 
-    {
+    }
+    else if (command === 'beep') {
         message.channel.send('boop');
     }
-    else if (command === 'args-info') 
-    {
+    else if (command === 'wiki') {
         if (!args.length) {
             return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
         }
-    
-        message.channel.send(`Command name: ${command}\nArguments: ${args}`);
+        let url = `https://en.wikipedia.org/w/api.php`;
+
+        const params = {
+            action: "opensearch",
+            search: args.join(' '),
+            limit: "5",
+            namespace: "0",
+            format: "json"
+        }
+
+        url = url + "?origin=*";
+        Object.keys(params).forEach(function (key) { url += "&" + key + "=" + params[key]; });
+
+        superagent
+            .get(url)
+            .then((res) => {message.channel.send(res.body[3][0])})
+            .catch((error) => {console.log(error)})
+
+        //message.channel.send(`Command name: ${command}\nArguments: ${args.join(' ')}`);
     }
     // other commands...
 });
