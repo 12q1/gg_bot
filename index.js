62725048fc6e6169af4a162c09e197c0504bd3b9
12q1 <thomasham89@gmail.com>
2020-10-17 22:53:59 +0200

    added dadjoke command

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
-const { search } = require('superagent');
 const superagent = require('superagent');
-const { prefix, token } = require('./config.json');
+const { prefix, token, giphykey } = require('./config.json');
 const client = new Discord.Client();
 
 client.on('ready', () => {
@@ -20,9 +19,9 @@ client.on('message', message => {
     else if (command === 'beep') {
         message.channel.send('boop');
     }
-    else if (command === 'wiki') {
+    else if (command === 'wiki') { //makes a request to wikipedia open search and returns the first hit
         if (!args.length) {
-            return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+            return message.channel.send(`You didn't provide any search keywords ${message.author}!`);
         }
         let url = `https://en.wikipedia.org/w/api.php`;
 
@@ -39,12 +38,27 @@ client.on('message', message => {
 
         superagent
             .get(url)
-            .then((res) => {message.channel.send(res.body[3][0])})
-            .catch((error) => {console.log(error)})
-
-        //message.channel.send(`Command name: ${command}\nArguments: ${args.join(' ')}`);
+            .then((res) => { message.channel.send(res.body[3][0]) })
+            .catch((error) => { console.log(error) })
     }
-    // other commands...
+    else if (command === 'gif') { //makes a request to the giphy random API
+        if (!args.length) { //if there are no keywords then we just get a random gif
+            url = `https://api.giphy.com/v1/gifs/random?api_key=${giphykey}&tag=funny&rating=r`;
+
+            superagent
+                .get(url)
+                .then(res => message.channel.send(res.body.data.image_url))
+                .catch(error => console.log(error))
+        }
+        else { //otherwise we use the giphy translate feature to get a reaction gif
+            url = `https://api.giphy.com/v1/gifs/translate?api_key=${giphykey}&s=${args.join(' ')}`
+
+            superagent
+                .get(url)
+                .then(res => message.channel.send(res.body.data.url))
+                .catch(error => console.log(error))
+        }
+    }
 });
 
 client.login(token);
