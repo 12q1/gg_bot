deb86cb46db4f8c2d5f90c53d7944810b0dc5a87
12q1 <thomasham89@gmail.com>
2020-10-18 02:29:57 +0200

    added a reminder command

--- index.js
+++ index.js
@@ -126,6 +126,19 @@ client.on('message', message => {
                 .catch(error => console.log(error))
         }
     }
+    else if (command === 'remind') {
+        if (!args.length) {
+            message.channel.send("You didn't provide any arguments")
+        }
+        else {
+            const minutes = args[0];
+            args.shift()
+            message.channel.send(`${message.author}, you will receive a reminder in ${minutes} minute(s)`)
+            setTimeout(() => {
+                message.channel.send(`${args.join(" ")}`)
+            }, 60000 * minutes)
+        }
+    }
 });
 
 client.login(token);
