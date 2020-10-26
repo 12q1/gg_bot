c39c36d05712dc76a6b562db963badd003ddc1b5
12q1 <thomasham89@gmail.com>
2020-10-26 13:47:06 +0100

    added an advice command

---
+++ commands/advice.js
@@ -0,0 +1,19 @@
+const superagent = require('superagent');
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'advice',
+    description: 'Advice',
+    execute(message, args) {
+        superagent
+            .get('https://api.adviceslip.com/advice')
+            .then(res => {
+                const data =JSON.parse(res.text)
+                const adviceEmbed = new Discord.MessageEmbed()
+                    .setColor('#0099ff')
+                    .setDescription(data.slip.advice)
+                message.channel.send(adviceEmbed)
+            })
+            .catch(error => console.log(error))
+    },
+};
